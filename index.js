6b3218078f559e3c7a416f93b3ecff8ac2f539cf
BestLeaks <59940456+BestLeaks@users.noreply.github.com>
2023-06-03 02:33:54 -0400

    add inisity-questions to the options
    Signed-off-by: BestLeaks <59940456+BestLeaks@users.noreply.github.com>

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log('Thanks for using questions-and-facts Questions Bundle!')
 // Importing files
 // Fact Files
 const wyr = require('./directory/would-you-rather.json')
-const carFacts = require('./directory/car-facts.json')
+const inisityQuestions = require('./directory/inisity-questions.json')
 const foodFacts = require('./directory/food-facts.json')
 
 // Code starts
@@ -12,13 +12,13 @@ const foodFacts = require('./directory/food-facts.json')
 function question () {
   
   let allQuestions = [
-    wyr[Math.floor(Math.random() * wyr.length)], carFacts[Math.floor(Math.random() * spaceFacts.length)], foodFacts[Math.floor(Math.random() * foodFacts.length)]
+    wyr[Math.floor(Math.random() * wyr.length)], inisityQuestions[Math.floor(Math.random() * spaceFacts.length)], foodFacts[Math.floor(Math.random() * foodFacts.length)]
   ]
 
   return{
     all: allQuestions[Math.floor(Math.random() * allFacts.length)],
     wyr: animalFacts[Math.floor(Math.random() * wyr.length)],
-    car: carFacts[Math.floor(Math.random() * carFacts.length)],
+    inisityQuestions: inisityQuestions[Math.floor(Math.random() * inisityQuestions.length)],
     food: foodFacts[Math.floor(Math.random() * foodFacts.length)]
   }
 }
